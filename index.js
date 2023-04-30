96003b3c9e4e97aaea874c1a8bec84dbeed6ea35
shuvakarmakar <shuva15-7953@diu.edu.bd>
2023-05-01 00:24:43 +0600

    SOme Uptades

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ var cors = require('cors')
 
 app.use(cors())
 
-const categories = require('./data/categories.json')
+const categories = require('./data/categories.json');
+
+const news = require('./data/news.json')
 
 app.get('/', (req, res) =>{
     res.send('Dragon is Running')
@@ -14,6 +16,29 @@ app.get('/', (req, res) =>{
 
 app.get('/categories', (req, res) =>{
     res.send(categories)
+})
+
+app.get('/news', (req, res) =>{
+    res.send(news)
+})
+
+app.get('/news/:id', (req, res) =>{
+    const id = req.params.id;
+    console.log(id)
+    const selecetedNews = news.find(n => n._id === id);
+    res.send(selecetedNews);
+})
+
+app.get('/category/:id', (req, res) =>{
+    const id = req.params.id;
+    console.log(id);
+    if(id == 0){
+        res.send(news);
+    }
+    else{
+        const categoryNews = news.filter(n => n.category_id == id);
+        res.send(categoryNews)
+    }
 })
 
 app.listen(port, () => {
